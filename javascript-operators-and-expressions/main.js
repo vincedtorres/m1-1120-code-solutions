327c257f12b597d15c19664dbc640a049350ee6f
Vincent Torres <vincedtorres1984@gmail.com>
2020-12-15 11:31:31 -0800

    Completed JavaScript Operators and Expressions.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,38 @@
+var width = 23;
+var height = 15;
+var area = width * height;
+console.log('value of area', area);
+console.log('typeof area:', typeof area);
+
+var bill = 65;
+var payment = 80;
+var change = bill - payment;
+console.log('value of change', change);
+console.log('typeof change', typeof change);
+
+var quizes = 98;
+var midterm = 80;
+var final = 89;
+var grade = (quizes + midterm + final) / 3;
+console.log('value of grade', grade);
+console.log('typeof grade', typeof grade);
+
+var firstName = 'Vincent ';
+var lastName = 'Torres';
+var fullName = firstName + lastName;
+console.log('value of fullName', fullName);
+console.log('typeof fullName', typeof fullName);
+
+var ph = 5;
+var isAcidic = ph < 7;
+console.log('value of isAcidic', isAcidic);
+console.log('typeof isAcidic', typeof isAcidic);
+
+var headCount = 463;
+var isSparta = (headCount === 300);
+console.log('value of isSparta', isSparta);
+console.log('typeof isSparta', typeof isSparta);
+
+var motto = 'Dave Chappelle' + ' is the GOAT';
+console.log('value of motto', motto);
+console.log('typeof motto', typeof motto);
